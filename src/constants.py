226df580eb2df4054f90e5efe44815188d1bcb3d
miguel <migue8gl@gmail.com>
2024-02-17 18:00:47 +0100

    Fix

--- src/constants.py
+++ src/constants.py
@@ -17,9 +17,9 @@ PLOT_TITLE = 'Metaheuristic optimization analysis visualization'
 
 DEFAULT_OPTIMIZER = 'ACO'
 DEFAULT_TEST_ITERATIONS = 100  # Testing purposes, can be changed
-DEFAULT_ITERATIONS = 3  # Analisys iterations for each optimizer
-DEFAULT_MAX_ITERATIONS = 2  # For analisys comparison between optimizers
-DEFAULT_POPULATION_SIZE = 2  # Vector solution length
+DEFAULT_ITERATIONS = 500  # Analisys iterations for each optimizer
+DEFAULT_MAX_ITERATIONS = 30  # For analisys comparison between optimizers
+DEFAULT_POPULATION_SIZE = 20  # Vector solution length
 DEFAULT_FOLDS = 5  # Number of folds for k-fold cross validation
 DEFAULT_NEIGHBORS = 10  # Number of neighbors for KNN
 DEFAULT_LOWER_BOUND = 0  # Min value for features in solution
