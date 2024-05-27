dd6bf0bf774759fa28dcda4432d01fea016a2ea7
miguel <migue8gl@gmail.com>
2024-05-27 17:56:34 +0200

    Statistic script in R for testing results

---
+++ src/stats_test.r
@@ -0,0 +1,17 @@
+library(rio)
+library(dplyr)
+
+# Import data
+df_binary <- import("results/binary/analysis_results.csv")
+df_real <- import("results/real/analysis_results.csv")
+
+# Perform pairwise Wilcoxon rank sum tests and save results
+metrics <- c("selected_rate", "acc", "avg", "execution_time")
+for (metric in metrics) {
+    write.csv(pairwise.wilcox.test(df_binary[[metric]], df_binary$optimizer, p.adjust.method = "holm")$p.value,
+        file = paste0("results/stats/wilcox_test_", metric, "_binary.csv")
+    )
+    write.csv(pairwise.wilcox.test(df_real[[metric]], df_real$optimizer, p.adjust.method = "holm")$p.value,
+        file = paste0("results/stats/wilcox_test_", metric, "_real.csv")
+    )
+}
